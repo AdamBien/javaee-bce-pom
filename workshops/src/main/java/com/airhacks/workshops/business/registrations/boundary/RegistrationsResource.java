4f48ec0fd4d3a7ef0c5fc33c6aa554f4b803feba
Adam Bien <abien@adam-bien.com>
2014-09-12 11:33:54 +0200

    Produces / Consumes mime types added

--- workshops/src/main/java/com/airhacks/workshops/business/registrations/boundary/RegistrationsResource.java
+++ workshops/src/main/java/com/airhacks/workshops/business/registrations/boundary/RegistrationsResource.java
@@ -7,11 +7,14 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -20,6 +23,8 @@ import javax.ws.rs.core.UriInfo;
  * @author airhacks.com
  */
 @Stateless
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 @Path("registrations")
 public class RegistrationsResource {
 
